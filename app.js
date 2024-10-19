573c2390975602a302b85d417ef51d7b6b27de37
AliMahmoudi <alimahmoudi21a@gmail.com>
2024-10-19 18:00:26 +0330

    Add mobile menu and its js

---
+++ app.js
@@ -0,0 +1,13 @@
+// **mobile menu**
+const headerBtn = document.querySelector(".header__menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileBtn = document.querySelector(".mobile__logo svg");
+
+headerBtn.addEventListener("click",() => {
+    mobileMenu.classList.add("mobile-menu--active");
+});
+mobileBtn.addEventListener("click",() => {
+    mobileMenu.classList.remove("mobile-menu--active");
+});
+
+// **menu link hover**
